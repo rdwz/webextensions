13608f5c3469a7676f1b99a017aeba32b6f0e9b5
Marnes <leaumar@cronos.be>
2022-02-15 14:16:48 +0100

    vue poc

--- extensions/public-ip-display/src/popup.tsx
+++ extensions/public-ip-display/src/popup.tsx
@@ -1,22 +1,5 @@
-import React, { ReactElement } from "react";
-import ReactDOM from "react-dom";
-import { DataSection } from "./popup/data-section";
-import { useLastResult } from "./popup/state/last-result";
-import { LogSection } from "./popup/log-section";
-import { RefreshHeader } from "./popup/refresh-header";
+import { createApp } from "vue";
+import thing from "./thing.vue";
 
-function Popup(): ReactElement {
-    const lastResult = useLastResult();
-
-    return (
-        <>
-            <RefreshHeader lastResult={lastResult} />
-
-            {lastResult == null ? null : <DataSection data={lastResult} />}
-
-            <LogSection />
-        </>
-    );
-}
-
-ReactDOM.render(<Popup />, document.body);
+const app = createApp(thing);
+app.mount("body");
---
+++ extensions/public-ip-display/src/thing.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    // Properties returned from data() becomes reactive state
+    // and will be exposed on `this`.
+    data() {
+        return {
+            count: 0,
+            name: "thing",
+        };
+    },
+
+    // Methods are functions that mutate state and trigger updates.
+    // They can be bound as event listeners in templates.
+    methods: {
+        increment() {
+            this.count++;
+        },
+    },
+
+    // Lifecycle hooks are called at different stages
+    // of a component's lifecycle.
+    // This function will be called when the component is mounted.
+    mounted() {
+        console.log(`The initial count is ${this.count}.`);
+    },
+};
+</script>
+
+<template>
+    <button @click="increment">count is: {{ count }}</button>
+</template>
